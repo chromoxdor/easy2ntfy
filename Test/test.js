7c6a08cfa1e799e2d7e9ed7f7d058d3fa50b123c
chromoxdor <33860956+chromoxdor@users.noreply.github.com>
2023-04-24 12:28:07 +0200

    test

--- Test/test.js
+++ Test/test.js
@@ -334,7 +334,7 @@ function testlocal(ip, hostN) {
         this.img.onerror = function (e) {
             if (_that.inUse) {
                 _that.inUse = false;
-                console.log('responded');
+                console.log('responded2');
                 window.open("http://" + ip, "_self")
             }
 
